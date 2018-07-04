79efa729c909263a43fb5730c697962ce5739ae7
Quentin Bonaventure <quentin@bonaventure.xyz>
2018-07-04 16:38:24 +0200

    WIP

--- public/run.php
+++ public/run.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 use FTCBotCore\Broker\BrokerClient;
-use FTC\Discord\Message;
+use FTCBotCore\Message\Message;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
