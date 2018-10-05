c1e5a1f874a6d81c57119cb50df2776b1e28bcec
Quentin Bonaventure <quentin@bonaventure.xyz>
2018-10-05 10:49:33 +0200

    Adds missing MessageDeleteBulk Message class

---
+++ src/Message/MessageDeleteBulk.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FTCBotCore\Message;
+
+use FTCBotCore\Message\Message;
+
+class MessageDeleteBulk extends Message
+{
+    
+    const EVENT_NAME = 'MESSAGE_DELETE_BULK';
+    
+}
